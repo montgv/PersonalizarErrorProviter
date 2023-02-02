fff18b2cee095014141a43ca99b9d79023d7cc4e
Montse <montserrat.garrido.220@estudiante.iesoretania.es>
2023-02-02 17:57:42 +0100

    Agregar archivos de proyecto.

---
+++ PersonalizarErrorProviter/ErrorProviterEmail.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace PersonalizarErrorProviter
+{
+    partial class ErrorProviterEmail
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de componentes
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 0);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 0;
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
---
+++ PersonalizarErrorProviter/ErrorProviterEmail.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonalizarErrorProviter
+{
+    public partial class ErrorProviterEmail: TextBox
+    {
+        public ErrorProviterEmail()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnTextChanged(EventArgs e)
+        {
+            base.OnTextChanged(e);
+            //Si la funcion devuelte falso el fondo del textbox es rojo, cuando devuelve true se pinta de verde
+            if (!ValidarEmail(this.Text))
+            {
+                this.BackColor = Color.Red;
+            }
+            else
+            {
+                this.BackColor = Color.Green;
+            }
+        }
+
+        //Funcion que valida que email contenga una @ y un ., devuelve true cuando se cumple
+        private bool ValidarEmail(string email)
+        {
+            if (email.Length == 0)
+            {
+                return false;
+            }
+
+            if (email.IndexOf("@") > -1)
+            {
+                if (email.IndexOf(".", email.IndexOf("@")) > email.IndexOf("@"))
+                {
+                    return true;
+                }
+            }
+            return false;
+
+            throw new NotImplementedException();
+        }
+    }
+}
